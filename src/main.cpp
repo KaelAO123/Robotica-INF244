d4555811550add33b7288ac5219d006be2211ffd
Carlos Reyes <caereyes0@gmail.com>
2025-01-16 18:45:54 -0400

    Anotaciones extras

--- src/main.cpp
+++ src/main.cpp
@@ -1,110 +1,52 @@
 #include <Arduino.h>
-#include <LiquidCrystal_I2C.h>
-byte red = 8, green = 9, blue = 10;
-
-byte PULS1 = 7;
-byte PULS2 = 6;
-int num;
-
-byte POTEN = A1;
-int val = 0;
-void setup()
-{
-    pinMode(red, OUTPUT);
-    pinMode(green, OUTPUT);
-    pinMode(blue, OUTPUT);
+byte Ain1=7, Ain2=8, Bin1=4, Bin2=5;
+byte PWMA=9, PWMB=6;
+void setup(){
+    pinMode(Ain1, OUTPUT);
+    pinMode(Ain2, OUTPUT);
+    pinMode(Bin1, OUTPUT);
+    pinMode(Bin2, OUTPUT);
+  	pinMode(PWMA, OUTPUT);
+    pinMode(PWMA, OUTPUT);
 }
 
-void loop()
-{
-    int cont = 0;
-    int valorPuls1 = digitalRead(PULS1);
-    int valorPuls2 = digitalRead(PULS2);
+void loop(){
+  digitalWrite(Ain1, 0);
+    digitalWrite(Ain2, 1);
+    digitalWrite(Bin1, 0);
+    digitalWrite(Bin2, 1);
+    analogWrite(PWMA, 255);
+    analogWrite(PWMB, 255);
+    delay(2000);
+    digitalWrite(Ain1, 1);
+    digitalWrite(Ain2, 0);
+    digitalWrite(Bin1, 1);
+    digitalWrite(Bin2, 0);
+    analogWrite(PWMA, 255);
+    analogWrite(PWMB, 255);
+    delay(2000);
+    
+    digitalWrite(Ain1, 0);
+    digitalWrite(Ain2, 1);
+    digitalWrite(Bin1, 1);
+    digitalWrite(Bin2, 0);
+    analogWrite(PWMA, 0);
+    analogWrite(PWMB, 255);
+    delay(2000);
+    digitalWrite(Ain1, 1);
+    digitalWrite(Ain2, 0);
+    digitalWrite(Bin1, 0);
+    digitalWrite(Bin2, 1);
+    analogWrite(PWMA, 255);
+    analogWrite(PWMB, 0);
+    delay(2000);
 
-    if (valorPuls1)
-    {
-        num = random(0, 6);
-        for (size_t i = 0; i < num; i++)
-        {
-            digitalWrite(red, 1);
-            digitalWrite(green, 0);
-            digitalWrite(blue, 0);
-
-            delay(500);
-            
-            digitalWrite(red, 0);
-            digitalWrite(green, 0);
-            digitalWrite(blue, 0);
-
-            delay(500);
-
-            digitalWrite(red, 0);
-            digitalWrite(green, 1);
-            digitalWrite(blue, 0);
-
-            delay(500);
-
-            digitalWrite(red, 0);
-            digitalWrite(green, 0);
-            digitalWrite(blue, 0);
-
-            delay(500);
-
-            digitalWrite(red, 0);
-            digitalWrite(green, 1);
-            digitalWrite(blue, 1);
-
-            delay(500);
-
-            digitalWrite(red, 0);
-            digitalWrite(green, 0);
-            digitalWrite(blue, 0);
-
-            delay(500);
-
-        }
-    }
-    else if (valorPuls2)
-    {
-        num = random(0, 6);
-        for (size_t i = 0; i < num; i++)
-        {
-            digitalWrite(red, 1);
-            digitalWrite(green, 1);
-            digitalWrite(blue, 0);
-
-            delay(500);
-            
-            digitalWrite(red, 0);
-            digitalWrite(green, 0);
-            digitalWrite(blue, 0);
-
-            delay(500);
-
-            digitalWrite(red, 1);
-            digitalWrite(green, 0);
-            digitalWrite(blue, 1);
-
-            delay(500);
-
-            digitalWrite(red, 0);
-            digitalWrite(green, 0);
-            digitalWrite(blue, 0);
-
-            delay(500);
-
-            digitalWrite(red, 0);
-            digitalWrite(green, 1);
-            digitalWrite(blue, 1);
-
-            delay(500);
-
-            digitalWrite(red, 0);
-            digitalWrite(green, 0);
-            digitalWrite(blue, 0);
-
-            delay(500);
-
-        }
-    }
+    digitalWrite(Ain1, 0);
+    digitalWrite(Ain2, 0);
+    digitalWrite(Bin1, 0);
+    digitalWrite(Bin2, 0);
+    analogWrite(PWMA, 0);
+    analogWrite(PWMB, 0);
+    delay(2000);
+    
 }
