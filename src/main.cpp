255fb860aeb5895a6c86d706a7f2caaebeec580f
Carlos Reyes <caereyes0@gmail.com>
2025-01-18 19:05:36 -0400

    Motores

--- src/main.cpp
+++ src/main.cpp
@@ -1,52 +1,23 @@
 #include <Arduino.h>
-byte Ain1=7, Ain2=8, Bin1=4, Bin2=5;
-byte PWMA=9, PWMB=6;
-void setup(){
-    pinMode(Ain1, OUTPUT);
-    pinMode(Ain2, OUTPUT);
-    pinMode(Bin1, OUTPUT);
-    pinMode(Bin2, OUTPUT);
-  	pinMode(PWMA, OUTPUT);
-    pinMode(PWMA, OUTPUT);
+#include <LiquidCrystal_I2C.h>
+#include <SoftwareSerial.h>   // Incluimos la libreria SoftwareSerial
+SoftwareSerial Bluetooth(10,11);    // Definimos los pines RX=11 y TX=10 del Arduino conectados al Bluetooth
+void setup()
+{
+  Bluetooth.begin(38400);       // Fijamos la velocidad de comunicacion del arduino con el Bluetooth 
+  Serial.begin(9600);   // Fijamos la velocidad de comunicacion de el arduido con la PC
+
+  Serial.println("Innovadores");
 }
-
-void loop(){
-  digitalWrite(Ain1, 0);
-    digitalWrite(Ain2, 1);
-    digitalWrite(Bin1, 0);
-    digitalWrite(Bin2, 1);
-    analogWrite(PWMA, 255);
-    analogWrite(PWMB, 255);
-    delay(2000);
-    digitalWrite(Ain1, 1);
-    digitalWrite(Ain2, 0);
-    digitalWrite(Bin1, 1);
-    digitalWrite(Bin2, 0);
-    analogWrite(PWMA, 255);
-    analogWrite(PWMB, 255);
-    delay(2000);
-    
-    digitalWrite(Ain1, 0);
-    digitalWrite(Ain2, 1);
-    digitalWrite(Bin1, 1);
-    digitalWrite(Bin2, 0);
-    analogWrite(PWMA, 0);
-    analogWrite(PWMB, 255);
-    delay(2000);
-    digitalWrite(Ain1, 1);
-    digitalWrite(Ain2, 0);
-    digitalWrite(Bin1, 0);
-    digitalWrite(Bin2, 1);
-    analogWrite(PWMA, 255);
-    analogWrite(PWMB, 0);
-    delay(2000);
-
-    digitalWrite(Ain1, 0);
-    digitalWrite(Ain2, 0);
-    digitalWrite(Bin1, 0);
-    digitalWrite(Bin2, 0);
-    analogWrite(PWMA, 0);
-    analogWrite(PWMB, 0);
-    delay(2000);
-    
+ 
+void loop()
+{ 
+  if(Bluetooth.available())    // Si llega un dato por el puerto Bluetooth se imprime en el monitor serial
+  {
+    Serial.write(Bluetooth.read());
+  }
+  if(Serial.available())  // Si se escribe y envia un dato por el monitor serial, se le manda al Bluetooth 
+  {
+     Bluetooth.write(Serial.read());
+  }
 }
